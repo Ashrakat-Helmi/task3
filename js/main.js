3f07d64e30fe7acd28f9108eafd914f10d3d28ef
Ashrakat-Helmi <ashrakat5elkady@gmail.com>
2021-09-05 13:10:02 +0200

    first edit

--- js/main.js
+++ js/main.js
@@ -1,34 +1,48 @@
-let input = document.querySelectorAll('input')
-let btn = document.querySelectorAll('button')
-let div = document.querySelectorAll('div')
-let body = document.querySelector('body')
-let nav =document.querySelector('nav')
-let form =document.querySelector('form')
-let addStyle =() => {
-    w = input[0].value;
-    h = input[1].value;
-    bgc = input[2].value;
+let btnMood =document.getElementById('btnMood');
+let body =document.querySelector('body');
+let style=document.getElementById('style');
+let form =document.querySelector('div .form1');
+let nav =document.querySelector('nav');
+let wInput=document.getElementById('width');
+let hInput =document.getElementById('Hight');
+let bgc =document.getElementById('backGroundColor');
+let styleAdded=document.getElementById('styleAdded');
+let btnStyle =document.getElementById('done');
 
-    div[8].style.width = w;
-    div[8].style.height = h;
-    div[8].style.backgroundColor = bgc;
+
+
+
+let addStyle =() =>{
+    w = wInput.value;
+    h = hInput.value;
+    bg =bgc.value;
+
+    styleAdded.style.width =w;
+    styleAdded.style.height =h;
+    styleAdded.style.backgroundColor =bg;
 }
-btn[1].addEventListener('click', addStyle);
 
-btn[0].onclick = function(){
+btnStyle.addEventListener('click',addStyle );
+
+
+
+
+
+
+btnMood.onclick = function(){
     if(body.classList.contains('dark')){
         body.classList.remove('dark')
-        btn[0].classList = " btn btn-dark"
-        btn[0].innerText ="Dark M0od"
+        btnMood.classList = " btn btn-dark"
+        btnMood.innerText ="Dark M0od"
         nav.classList= "navbar navbar-expand-lg navbar-light bg-light"
-        div[7].style.borderColor ='black'
+        style.style.borderColor ='black'
         form.style.borderColor ='black'
     }else{
         body.classList.add('dark')
-        btn[0].classList = " btn btn-light"
-       btn[0].innerText ="Light M0od"
+        btnMood.classList = " btn btn-light"
+       btnMood.innerText ="Light M0od"
        nav.classList= "navbar navbar-expand-lg navbar-dark bg-dark"
-       div[7].style.borderColor ='white'
+       style.style.borderColor ='white'
        form.style.borderColor ='white'
     }
 }
